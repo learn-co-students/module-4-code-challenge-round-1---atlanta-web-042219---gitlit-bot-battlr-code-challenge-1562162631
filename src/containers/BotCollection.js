cb9f9fed17b2b2f97292a721ecca64fcf0dcea2a
Evan Kim <dkim2435@gmail.com>
2019-07-03 10:29:34 -0400

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,20 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {/*...and here..*/}
+          {this.props.bots.map(bot => (
+            <div onClick={e => this.props.addBot(bot)}>
+              <BotCard bot={bot} key={bot.id} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,47 @@
 import React from "react";
-
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 class BotsPage extends React.Component {
   //start here with your code for step one
+
+  state = { bots: [], userBots: [] };
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(bots => this.setState({ bots: bots }));
+  }
+
+  addBot = bot => {
+    let newUserBots = this.state.userBots;
+    const findBot = newUserBots.find(findBot => findBot.id === bot.id);
+
+    if (!findBot) {
+      newUserBots.push(bot);
+      this.setState({ userBots: newUserBots });
+    }
+  };
+
+  removeBot = bot => {
+    let delUserBots = this.state.userBots;
+    let index = delUserBots.indexOf(bot);
+    delUserBots.splice(index, 1);
+
+    this.setState({ userBots: delUserBots });
+  };
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          userBots={this.state.userBots}
+          removeBot={this.removeBot}
+        />
+        <BotCollection bots={this.state.bots} addBot={this.addBot} />
       </div>
     );
   }
-
 }
 
 export default BotsPage;
